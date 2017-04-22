e9a8d852d3947b4eb3161b18b8a2ecda6926a40b
Azis Adi Kuncoro <azisadikuncoro@gmail.com>
2017-04-22 23:09:24 +0700

    menambah kelas Point

---
+++ src/point/Point.java
@@ -0,0 +1,36 @@
+package point;
+
+/**
+ * Created by AZIS ADI KUNCORO (13515120) on 22/04/2017.
+ */
+public class Point {
+  private int absis;
+  private int ordinat;
+
+  public Point(int x, int y) {
+    absis = x;
+    ordinat = y;
+  }
+
+  public Point(final Point p) {
+    absis = p.absis;
+    ordinat = p.ordinat;
+  }
+
+  /* ==GETTER== */
+
+  public final int getX() {
+    return absis;
+  }
+
+  public final int getY() {
+    return ordinat;
+  }
+
+  /* ========= */
+
+  public final void display() {
+    System.out.println("[ " + getX() + " , " + getY() + " ]");
+  }
+
+}
