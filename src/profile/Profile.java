0041c34cdb754b3c608b71a46d94d09ddd6f08a5
Azis Adi Kuncoro <azisadikuncoro@gmail.com>
2017-04-22 23:06:39 +0700

    benerin kelas profile

--- src/profile/Profile.java
+++ src/profile/Profile.java
@@ -10,5 +10,59 @@ public class Profile {
   private int jumlahAnimal;
   private String[] achievement;
 
-  public Profile (String namaPemilik, long uang, String namaZoo, String[] achievement )
+  /**
+   * Konstruktor parameter dari kelas Profile
+   * @param namaPemilik nama dari pemilik zoo
+   * @param uang jumlah uang yang dimiiliki
+   * @param namaZoo nama dari sebuah zoo
+   * @param jumlahAnimal jumlah animal yang dimiliki
+   * @param achievement achievement - achievement yang dimiliki
+   */
+  public Profile (String namaPemilik, long uang, String namaZoo, int jumlahAnimal, String[] achievement){
+    this.namaPemilik = namaPemilik;
+    this.uang = uang;
+    this.namaZoo = namaZoo;
+    this.jumlahAnimal = jumlahAnimal;
+    this.achievement = achievement;
+  }
+
+  /**
+   * Getter nama pemilik
+   * @return namaPemilik
+   */
+  public String getNamaPemilik() {
+    return namaPemilik;
+  }
+
+  /**
+   * Getter uang
+   * @return uang
+   */
+  public long getUang() {
+    return uang;
+  }
+
+  /**
+   * Getter nama zoo
+   * @return namaZoo
+   */
+  public String getNamaZoo() {
+    return namaZoo;
+  }
+
+  /**
+   * Getter jumlah animal
+   * @return jumlahAnimal
+   */
+  public int getJumlahAnimal() {
+    return jumlahAnimal;
+  }
+
+  /**
+   * Getter achievement
+   * @return achievement
+   */
+  public String[] getAchievement() {
+    return achievement;
+  }
 }
